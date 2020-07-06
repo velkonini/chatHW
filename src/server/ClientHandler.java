1f56a0f0c10f36f70bcb484805e12abb07b9613e
velkonini <avelkov07@gmail.com>
2020-07-06 21:07:34 +0300

    Initial commit

---
+++ src/server/ClientHandler.java
@@ -0,0 +1,101 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    Server server;
+    Socket socket = null;
+    DataInputStream in;
+    DataOutputStream out;
+
+    private String nick;
+    private String login;
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    //цикл аутентификации
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.startsWith("/auth ")) {
+                            String[] token = str.split("\\s");
+                            if (token.length < 3) {
+                                continue;
+                            }
+                            String newNick = server
+                                    .getAuthService()
+                                    .getNicknameByLoginAndPassword(token[1], token[2]);
+                            if (newNick != null) {
+                                sendMsg("/authok " + newNick);
+                                nick = newNick;
+                                login = token[1];
+                                server.subscribe(this);
+                                System.out.printf("Клиент %s подключился \n", nick);
+                                break;
+                            } else {
+                                sendMsg("Неверный логин / пароль");
+                            }
+                        }
+
+                        server.broadcastMsg(str);
+                    }
+                    //цикл работы
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.equals("/end")) {
+                            out.writeUTF("/end");
+                            break;
+                        }
+
+                        server.broadcastMsg(str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("Клиент отключился");
+                    server.unsubscribe(this);
+                    try {
+                        in.close();
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    void sendMsg(String str) {
+        try {
+            out.writeUTF(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNick() {
+        return nick;
+    }
+}
---
+++ src/server/Server.java
@@ -0,0 +1,66 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Server {
+    private List<ClientHandler> clients;
+    private AuthService authService;
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public Server() {
+        clients = new Vector<>();
+        authService = new SimpleAuthService();
+
+        ServerSocket server = null;
+        Socket socket;
+
+        final int PORT = 8189;
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                System.out.println("socket.getRemoteSocketAddress(): "+socket.getRemoteSocketAddress());
+                System.out.println("socket.getLocalSocketAddress() "+socket.getLocalSocketAddress());
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void broadcastMsg(String msg){
+        for (ClientHandler client : clients) {
+            client.sendMsg(msg);
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler){
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler){
+        clients.remove(clientHandler);
+    }
+
+}
